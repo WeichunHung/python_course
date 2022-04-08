bc6707948267f3f2a92d6518dba8d2b0b07e6853
WeichunHung <hungwei0117@gmail.com>
2022-04-08 15:20:23 +0800

    0408_update

--- crawling_link.py
+++ crawling_link.py
@@ -10,12 +10,21 @@ def getLink(url):
 
     for ele in box:
         h3 = ele.findAll('h3')
-        print(h3)
+        # print(h3)
 
         for ele_1 in h3:
+            topic_title = ele_1.find('a').get('title')
+            print(topic_title)
             topic_link = 'https://sports.ettoday.net' + ele_1.find('a').get('href')
             print(topic_link)
+            topic_set = set(topic_link)
+            print(set(topic_link))
+            # first_link = topic_link()
 
+            # html = requests.get(topic_link)
+            # article_obj = BeautifulSoup(html.text, features="html.parser")
+            # next_recomm = article_obj.findAll('div', {'class': 'part_pictxt_5 recomm-news clearfix'})
+            # print(next_recomm)
 
 
 getLink("https://sports.ettoday.net/sport-category/%E6%A3%92%E7%90%83")
