f03f813a567d3634560cd40fa4c3189e148e8ee5
WeichunHung <hungwei0117@gmail.com>
2022-04-28 00:04:55 +0800

    0428_update

--- web_scraping_ETtoday.py
+++ web_scraping_ETtoday.py
@@ -1,41 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
 import re  # p.23
+import datetime
+from dateutil import relativedelta
+from dateutil.easter import *
+from dateutil.rrule import *
+from dateutil.parser import *
+from datetime import *
+
+# d = datetime.strptime("2022-04-27", "%Y-%m-%d")
+# d2 = d - relativedelta.relativedelta(months=1)
+# last_time = parse('2022-04-27 22:08:00+08:00')
+
+now = parse(str(datetime.now()))
+oneDay = now - relativedelta.relativedelta(days=1)
+print('Present Time:',now)
+print('Posts from:',oneDay)
 
 pages = set()
 
+def prefix_str(str):
+    return str.strip().replace(u'\u3000', u' ')
+
 
 def getLinks(pageUrl):
-    global pages
-    html = requests.get(pageUrl)
-    bsObj = BeautifulSoup(html.text, features="html.parser")
-    for link in bsObj.findAll("a", href=re.compile('/news/')):
-        if 'href' in link.attrs:
-            if link.attrs['href'] not in pages:
-                newPage = link.attrs['href']
-                if newPage.startswith('//sports.ettoday'):
-                    newPage = 'https:' + newPage
-                if newPage.startswith('/news/'):
-                    newPage = 'https://sports.ettoday.net' + newPage
 
-                html = requests.get(newPage)
-                newPage_Obj = BeautifulSoup(html.text, features="html.parser")
-                newsTitle = newPage_Obj.select('h1.title')
-                for title in newsTitle:
-                    title = title.text
-                newsDate = newPage_Obj.select('time.date')
-                for date in newsDate:
-                    date = date.text
-                # newsTag = newPage_Obj.select('div.tag a')
-                # for tag in newsTag:
-                #     tag = tag.text
-                Page_dict = {'Title': title, 'Date': date, 'Link': newPage}
-                print(Page_dict)
-                pages.add(newPage)
-                getLinks(newPage)
-                # news_list = []
-                # news_list.append(Page_dict)
-                # print(news_list)
+    try:
+        pages
+        html = requests.get(pageUrl)
+        bsObj = BeautifulSoup(html.text, features="html.parser")
+
+        for link in bsObj.findAll("a", href=re.compile('^/news/\d+')):
+            if 'href' in link.attrs:
+                if link.attrs['href'] not in pages:
+                    newPage = link.attrs['href']
+                    if newPage.startswith('//sports.ettoday'):
+                        newPage = 'https:' + newPage
+                    if newPage.startswith('/news/'):
+                        newPage = 'https://sports.ettoday.net' + newPage
+                    html = requests.get(newPage)
+                    newPage_Obj = BeautifulSoup(html.text, features="html.parser")
+                    dateObj = newPage_Obj.find('time').get('datetime')
+                    string = str(dateObj)
+                    newsDate = parse(string[:19])
+                    if newsDate > oneDay:
+                        title = newPage_Obj.select('h1.title')[0].text
+                        date = newPage_Obj.select('time.date')[0].text
+                        if len(newPage_Obj.select('div.tag a')) > 0:
+                            tag = newPage_Obj.select('div.tag a')[0].text
+                        else:
+                            tag = 'None'
+                        Page_dict = {'Title': prefix_str(title), 'Date': prefix_str(date), 'Link': prefix_str(newPage),
+                                     'Tag': prefix_str(tag)}
+                        print(Page_dict)
+                        pages.add(link.attrs['href'])
+                        getLinks(newPage)
+                    else:
+                        return
+    except Exception as e:
+        print(e)
+    else:
+        getLinks("https://sports.ettoday.net")
+
 
 getLinks("https://sports.ettoday.net")
-
